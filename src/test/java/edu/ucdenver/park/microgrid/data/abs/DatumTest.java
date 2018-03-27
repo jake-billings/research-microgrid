ab49871c052431a4aceeb2c12d400cfe3fbc323b
jakebillings <jake@jakebillings.com>
2018-03-27 17:09:10 -0600

    Refactor for maven/junit

---
+++ src/test/java/edu/ucdenver/park/microgrid/data/abs/DatumTest.java
@@ -0,0 +1,23 @@
+package edu.ucdenver.park.microgrid.data.abs;
+
+import org.junit.jupiter.api.Test;
+
+class DatumTest {
+
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void addEdge() {
+    }
+
+    @Test
+    void getEdges() {
+    }
+
+    @Test
+    void getNodes() {
+    }
+}
---
+++ src/test/java/edu/ucdenver/park/microgrid/data/abs/GraphTest.java
@@ -0,0 +1,24 @@
+package edu.ucdenver.park.microgrid.data.abs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void addEdge() {
+    }
+
+    @Test
+    void getEdges() {
+    }
+
+    @Test
+    void getNodes() {
+    }
+}
