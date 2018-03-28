e53f319a6d419976e4f99b7d491f51cff67ea616
jakebillings <jake@jakebillings.com>
2018-03-28 11:54:05 -0600

    Refactor name of receiver agent; add TestSenderAgent

---
+++ src/main/java/edu/ucdenver/park/microgrid/agents/MicrogridReceiverAgent.java
@@ -0,0 +1,99 @@
+package edu.ucdenver.park.microgrid.agents;
+
+import edu.ucdenver.park.microgrid.live.LiveMicrogridGraph;
+import edu.ucdenver.park.microgrid.socketioserver.MicrogridSocketIOServer;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+
+/**
+ * MicrogridReceiverAgent
+ * <p>
+ * class: JADE agent
+ * <p>
+ * This is the Jade agent that receives data from other controller agents and passes it to our Socket.IO server
+ * <p>
+ * This is the entry point to the program.
+ * <p>
+ * This ties everything together.
+ */
+public class MicrogridReceiverAgent extends Agent {
+    /**
+     * liveGrid
+     * <p>
+     * LiveMicrogridGraph
+     * <p>
+     * see docs for LiveMicrogridGraph
+     * <p>
+     * this object accepts message events that we get from jade a processes them into
+     * a graph state
+     * <p>
+     * it also processes events and sends them to SocketIOServer (since SocketIOServer gets a copy and
+     * registers itself some event listeners)
+     * <p>
+     * The job of setup in this agent is to create a SocketIOServer with a reference to this liveGrid
+     * The job of this agent is to parse Message object from jade and call liveGrid.receiveMessage()
+     * the socket server will then receive those events and pass them to the frontend for rendering
+     */
+    LiveMicrogridGraph liveGrid = new LiveMicrogridGraph();
+
+    /**
+     * server
+     * <p>
+     * MicrogridSocketIOServer
+     * <p>
+     * see docs for MicrogridSocketIOServer
+     * <p>
+     * this object manages the Socket.IO connection to the frontend javascript that
+     * renders a map of this data
+     * <p>
+     * it has a reference to the liveGrid and registers its own event handlers
+     * <p>
+     * when we update liveGrid, server gets the update and forwards it as JSON to the frontend via Socket.io
+     */
+    MicrogridSocketIOServer server = new MicrogridSocketIOServer(liveGrid, (short) 4000);
+
+    /**
+     * setup()
+     * <p>
+     * setup this data receiver object by starting the Socket.IO server
+     */
+    protected void setup() {
+        //---Welcome Message---
+        //Print a message that says the agent is starting
+        System.out.print("MicrogridReceiverAgent Starting...");
+
+        //Start the socket.io server
+        //server.init();
+
+        //---Add Behaviors---
+        addBehaviour(new ReceiveBehavior());
+
+        //---Completion Message---
+        //Print a message that we finished setting up
+        System.out.println("Done.");
+        System.out.println("MicrogridReceiverAgent running as: " + getLocalName());
+    }
+
+    /**
+     * ReceiveBehavior
+     * <p>
+     * class
+     * private internal class: this class exists INSIDE the MicrogridReceiverAgent class and is private to it (it is used only in setup())
+     * behavior: this a JADE behavior class
+     * <p>
+     * This CyclicBehavior continuously reads messages via JADE's messaging system and passes the messages
+     * back into LiveMicrogridGraph
+     */
+    private class ReceiveBehavior extends CyclicBehaviour {
+        public void action() {
+            System.out.println("Receiveing...");
+            ACLMessage msg = receive();
+            if (msg != null)
+                System.out.println(" - " +
+                        myAgent.getLocalName() + " <- " +
+                        msg.getContent());
+            block();
+        }
+    }
+}
---
+++ src/main/java/edu/ucdenver/park/microgrid/agents/TestSenderAgent.java
@@ -0,0 +1,44 @@
+package edu.ucdenver.park.microgrid.agents;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import jade.lang.acl.ACLMessage;
+
+/**
+ * TestSenderAgent
+ *
+ * class: JADE agent
+ *
+ * test agent that sends a string to "jake" every 2 seconds
+ *
+ * todo remove because this isn't useful; it's just here for reference
+ */
+public class TestSenderAgent extends Agent {
+    protected void setup() {
+
+        //---Welcome Message---
+        //Print a message that says the agent is starting
+        System.out.print("TestSenderAgent Starting...");
+
+        //Start the socket.io server
+        //server.init();
+
+        //---Add Behaviors---
+        addBehaviour(new TickerBehaviour(this, 2000) {
+            public void onTick() {
+                System.out.println("Sending...");
+
+                ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                msg.setContent("bla...bla...bla");
+                msg.addReceiver(new AID("jake", AID.ISLOCALNAME));
+                send(msg);
+            }
+        });
+
+        //---Completion Message---
+        //Print a message that we finished setting up
+        System.out.println("Done.");
+        System.out.println("TestSenderAgent running as: " + getLocalName());
+    }
+}
