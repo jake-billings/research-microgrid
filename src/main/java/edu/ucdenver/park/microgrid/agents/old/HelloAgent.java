5fc2e4415f51f26fa995debabf0eb3b043a03400
jakebillings <jake@jakebillings.com>
2018-04-04 16:06:24 -0600

    Add hello and test sender agents back

---
+++ src/main/java/edu/ucdenver/park/microgrid/agents/old/HelloAgent.java
@@ -0,0 +1,25 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ */
+package edu.ucdenver.park.microgrid.agents.old;
+
+import jade.core.Agent;
+
+/**
+ * HelloAgent
+ *
+ * basic agent that demonstrates JADE is working
+ *
+ * todo remove this cause it doesn't do anything
+ *
+ * @author Jake Billings
+ */
+public class HelloAgent extends Agent
+{
+    protected void setup()
+    {
+        System.out.println("Hello World. ");
+        System.out.println("My name is "+ getLocalName());
+    }
+}
